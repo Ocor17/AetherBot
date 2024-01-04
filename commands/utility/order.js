540860c14a0747ad1ca41530445d72cffd13c005
Juan Oscar Cordova <24573993+Ocor17@users.noreply.github.com>
2024-01-04 02:25:51 -0700

    general order functionality

--- commands/utility/order.js
+++ commands/utility/order.js
@@ -1,11 +1,27 @@
 const { SlashCommandBuilder } = require('discord.js');
+const firestore = require('../../Firestore');
+const admin = require('firebase-admin');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('order')
-		.setDescription('request a generalized order'),
+		.setDescription('request a generalized order')
+		.addStringOption(option =>
+			option.setName('order')
+				.setDescription('the order')
+				.setRequired(true))
+		,
 	async execute(interaction) {
-		// interaction.guild is the object representing the Guild in which the command was run
-		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+		const order = interaction.options.getString('order');
+		console.log("TIMESTAMP",interaction.createdTimestamp);
+
+		firestore.addOrder(
+			order,
+			admin.firestore.Timestamp.now(), 
+			interaction.user.username,
+			interaction.user.id,
+			);
+
+		interaction.reply({ content: `sent order`, ephemeral: true });
 	},
 };
